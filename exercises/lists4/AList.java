d72a31a27580fb513363fa9f5937b8772f763ec4
sometims <31714646+sometims@users.noreply.github.com>
2018-10-30 15:41:13 +0800

    Update AList.java

--- exercises/lists4/AList.java
+++ exercises/lists4/AList.java
@@ -5,29 +5,34 @@
 public class AList {
     /** Creates an empty list. */
     public AList() {
+        int[] A=int [100];
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        A[A.length]=x;
+        A.length+=1;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return A[A.length-1];        
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return A[i];        
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return A.length;        
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        A.length-=1;
+        return A[A.length-1];
+        
     }
 } 
