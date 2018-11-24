adce7ef9d13f42089804021393e542969d5fc587
sometims <31714646+sometims@users.noreply.github.com>
2018-11-24 17:41:13 +0800

    Update ArrayMap.java

--- exercises/DIY/syntax1/ArrayMap.java
+++ exercises/DIY/syntax1/ArrayMap.java
@@ -12,35 +12,62 @@ import static org.junit.Assert.*;
  * An array based implementation of the Map61B class.
  */
 public class ArrayMap<K, V> implements Map61B<K, V> {
+    private int size;
+    private K[] keys;
+    private V[] values;  
     public ArrayMap() {
+        size = 0;
+        keys = new Object[100]；
+        values = new Object[100];
     }
 
     /** Returns the index of the given key if it exists,
      *  -1 otherwise. */
     private int keyIndex(K key) {
-        return 0;
+        for(int i = 0;i < K.length,i++){
+            if(keys[i].equals(key)){
+                return i;
+            }
+        }
+        return -1;
     }
 
 
     public boolean containsKey(K key) {
-        return false;
+        int i = keyIndex(key);
+        return i > -1;
     }
 
     public void put(K key, V value) {
-        
+        int i = keyIndex(key);
+        if(i != -1){
+            values[i] = value;
+        else
+            keys[keys.size] = key;
+            values[values.size] = value;
+            size = size + 1;
     }
 
     public V get(K key) {
-        return false;
+        if(containKey(key){
+          int i = keyIndex(key);
+          return values[i];
+        else 
+            return -1;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public List<K> keys() {
-        return null;
+        List<K> keylist = new ArrayList<K>();
+        for (int i = 0; i < size; i += 1) {
+            keylist.add(keys[i]);
+        }
+        return keylist;
     }
+    
 
     /*@Test
     public void test() {
